75cf7731ba8942259cb90fc2afb5e4bee675d2b3
icyJoseph <sephxd1234@gmail.com>
2018-10-06 01:38:20 +0200

    feat: add geo key to reducer and sagas

--- frontend/src/ducks/rootReducer/index.js
+++ frontend/src/ducks/rootReducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "../auth";
+import geo from "../geoLocation";
 
 const rootReducer = combineReducers({
-  auth
+  auth,
+  geo
 });
 
 export default rootReducer;
--- frontend/src/ducks/rootSaga/index.js
+++ frontend/src/ducks/rootSaga/index.js
@@ -1,7 +1,8 @@
 import { all, fork } from "redux-saga/effects";
 
 import { authSaga } from "../auth/saga";
+import { geoSaga } from "../geoLocation/saga";
 
 export default function* rootSaga() {
-  yield all([fork(authSaga)]);
+  yield all([fork(authSaga), fork(geoSaga)]);
 }
