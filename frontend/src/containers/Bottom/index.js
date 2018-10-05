02fa27ab65900af53db5bed0e55d018bd9ad5893
icyJoseph <sephxd1234@gmail.com>
2018-10-06 01:37:01 +0200

    feat: get current position

--- frontend/src/containers/Bottom/index.js
+++ frontend/src/containers/Bottom/index.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import MyLocation from "@material-ui/icons/MyLocation";
 import NavigateBefore from "@material-ui/icons/NavigateBefore";
 import Search from "@material-ui/icons/Search";
+
+import { getCurrentPosition } from "../../ducks/geoLocation";
+
 import { StyledButton } from "../../components/styled/Buttons";
 import { Pinned } from "../../components/styled/Pinned";
 import { curry } from "../../functional";
 
 const goBack = history => history.push("/");
 
-export const Bottom = ({ history }) => (
+export const Bottom = ({ history, getPos }) => (
   <Pinned>
     <StyledButton
       variant="fab"
@@ -21,10 +25,18 @@ export const Bottom = ({ history }) => (
     <StyledButton variant="fab" color="primary" aria-label="search">
       <Search fontSize="large" />
     </StyledButton>
-    <StyledButton variant="fab" color="primary" aria-label="search">
+    <StyledButton
+      variant="fab"
+      color="primary"
+      aria-label="search"
+      onClick={getPos}
+    >
       <MyLocation fontSize="large" />
     </StyledButton>
   </Pinned>
 );
 
-export default Bottom;
+export default connect(
+  undefined,
+  { getPos: getCurrentPosition }
+)(Bottom);
