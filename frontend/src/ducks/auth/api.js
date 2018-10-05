83113147114d0a0fd464d2465d69eff9ae6c0119
icyJoseph <sephxd1234@gmail.com>
2018-10-06 01:36:00 +0200

    chore: use config endpoints

--- frontend/src/ducks/auth/api.js
+++ frontend/src/ducks/auth/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
-
-export const backEnd =
-  process.env.NODE_ENV === "production"
-    ? `${process.env.REACT_APP_BACKEND}/token`
-    : "http://localhost:1337/token";
+import { token } from "../config/endpoints";
 
 export const getToken = () => {
   return axios
-    .get(backEnd, { headers: { "Content-Type": "application/json" } })
+    .get(token, { headers: { "Content-Type": "application/json" } })
     .then(({ data }) => ({ ...data }));
 };
