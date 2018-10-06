e2dbf746fabaffb278809a5fa7d65a9ccc24c568
icyJoseph <sephxd1234@gmail.com>
2018-10-07 00:18:04 +0200

    feat: show track name or number of stops

--- frontend/src/components/Marker/index.js
+++ frontend/src/components/Marker/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Paper from "@material-ui/core/Paper";
+
+import Typography from "@material-ui/core/Typography";
 import { curry } from "../../functional";
 
 import marker from "./marker.png";
 
-export const Marker = ({ id, callback }) => {
+export const Marker = ({ id, callback, track, reference, name }) => {
   const hook = document.getElementById(id);
   return (
     hook &&
     ReactDOM.createPortal(
-      <div onClick={curry(callback)(id)}>
+      <Paper
+        onClick={curry(callback)(id)}
+        elevation={4}
+        style={{
+          width: "100%",
+          padding: "2px",
+          borderRadius: "10px",
+          textAlign: "center"
+        }}
+      >
+        <Typography variant="subheading" align="center" color="inherit">
+          {track ? track : reference.filter(ref => ref.name === name).length}
+        </Typography>
         <img src={marker} alt="Marker" style={{ width: "30px" }} />
-      </div>,
+      </Paper>,
       hook
     )
   );
