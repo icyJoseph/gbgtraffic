935ff0c542456a7a860364137ac714a84d9457cf
icyJoseph <sephxd1234@gmail.com>
2018-10-06 20:30:19 +0200

    feat: add functional helpers

--- frontend/src/functional/index.js
+++ frontend/src/functional/index.js
@@ -4,3 +4,10 @@ export const createSelector = (selectors, mapToProps) => state => {
   const args = selectors.map(selector => selector(state));
   return mapToProps(...args);
 };
+
+export const map = (arr, fn) => arr.map(e => fn(e));
+export const pipe = (...funcs) => (...args) =>
+  funcs.reduce(
+    (prev, curr, index) => (index === 0 ? curr(...args) : curr(prev)),
+    args
+  );
