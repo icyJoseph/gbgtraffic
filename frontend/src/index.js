c659408d95f6e483239bd7cc4f5c5ff6cd445f37
icyJoseph <sephxd1234@gmail.com>
2018-10-07 00:15:27 +0200

    feat: provide MUI theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
 import Routes from "./routes";
 import "./index.css";
 import configureStore from "./ducks/config";
@@ -9,10 +11,22 @@ import { register } from "./serviceWorker";
 const storedState = localStorage.getItem("bus-app");
 const store = configureStore(storedState ? JSON.parse(storedState) : undefined);
 
+const dodgerBlue = "#1E90FF";
+const white = "#ffffff";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: dodgerBlue },
+    secondary: { main: white }
+  }
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 register();
