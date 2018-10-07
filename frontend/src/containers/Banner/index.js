deab094286bdeef02f7f912703128fd8fedacb12
icyJoseph <sephxd1234@gmail.com>
2018-10-07 19:41:41 +0200

    feat: show status of traffic request

--- frontend/src/containers/Banner/index.js
+++ frontend/src/containers/Banner/index.js
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import { selectServerDate, selectServerTime } from "../../ducks/traffic";
+import {
+  selectServerDate,
+  selectServerTime,
+  selectFetchingState
+} from "../../ducks/traffic";
 
 import { createSelector } from "../../functional";
+
+const isFetchingMessage = (fetching, time, date) =>
+  fetching ? "Fetching resources." : `Server time: ${time} - ${date}`;
 
 export class Banner extends Component {
   // Show different messages
   render() {
-    const { date, time } = this.props;
+    const { time, date, fetching, error } = this.props;
     return (
       <Paper style={{ width: "80%", maxWidth: "500px", margin: "10px auto" }}>
         <Typography variant="body1" color="inherit" align="center">
-          Server time: {time} - {date}
+          {!error
+            ? isFetchingMessage(fetching, time, date)
+            : "Error while fetching, please retry."}
         </Typography>
       </Paper>
     );
@@ -21,8 +30,13 @@ export class Banner extends Component {
 }
 
 export default connect(
-  createSelector([selectServerDate, selectServerTime], (date, time) => ({
-    date,
-    time
-  }))
+  createSelector(
+    [selectServerDate, selectServerTime, selectFetchingState],
+    (date, time, { fetching, error }) => ({
+      date,
+      time,
+      fetching,
+      error
+    })
+  )
 )(Banner);
